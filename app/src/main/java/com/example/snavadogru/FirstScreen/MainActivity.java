dd8cfa2d7ef27feff41a57edba938ab5602db76b
hodjabedir <55664488+hodjabedir@users.noreply.github.com>
2020-04-01 17:24:11 +0300

    bildirim ve kullanıcımaili halledildi.

--- app/src/main/java/com/example/snavadogru/FirstScreen/MainActivity.java
+++ app/src/main/java/com/example/snavadogru/FirstScreen/MainActivity.java
@@ -6,16 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.snavadogru.Options;
 import com.example.snavadogru.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.onesignal.OneSignal;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button bEnterace;
+    EditText editTextMail;
+    String mail;
+    FirebaseDatabase firebaseDatabase= FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference("mail");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //One signal bildirim göndermek için ilgili kod
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
         bEnterace = findViewById(R.id.button);
 
         bEnterace.setOnClickListener(this);
@@ -26,6 +42,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch(v.getId()){
             case (R.id.button):
+
+                editTextMail = findViewById(R.id.editText);
+                mail = editTextMail.getText().toString();
+                databaseReference.setValue(mail);                                  // realtime olrak kullanıcı maili firebase kaydedildi
                 Intent i= new Intent(MainActivity.this, Options.class);
                 startActivity(i);
                 break;
