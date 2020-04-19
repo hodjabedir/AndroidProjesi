6cb107997409aef96ec39b9bfcf84f2bf116bfb8
hodjabedir <55664488+hodjabedir@users.noreply.github.com>
2020-04-19 20:30:07 +0300

    rehberlik tamamlandı

--- app/src/main/java/com/example/snavadogru/KENDİNİDENE.java
+++ app/src/main/java/com/example/snavadogru/KENDİNİDENE.java
@@ -1,8 +1,8 @@
 package com.example.snavadogru;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class KENDİNİDENE extends AppCompatActivity {
 
@@ -11,4 +11,6 @@ public class KENDİNİDENE extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kendinidene);
     }
+
+
 }
